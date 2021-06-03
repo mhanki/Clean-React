28f5f0b40c3f5adfc43c5d8cc90c6f0089ff22a0
mhanki <meike.hankewicz@protonmail.com>
2021-06-03 08:45:35 +0200

    update fs mock

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -4,19 +4,11 @@ const fs = jest.createMockFromModule('fs')
 
 const unlink = (path) => dir.remove(path)
 
-const writeFile = (path, data) => new Promise((resolve, reject) => {
-  setTimeout(() => {
-    dir.set({...dir.get(), [path]: data})
-    resolve()
-  })
-})
-
 const existsSync = (path) => dir.get().hasOwnProperty(path)
 
 const readFileSync = (path) => dir.get()[path]
 
 fs.unlink = unlink
-fs.writeFile = writeFile
 fs.existsSync = existsSync
 fs.readFileSync = readFileSync
 
