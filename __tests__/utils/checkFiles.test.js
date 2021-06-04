47d06c48bd618d1a5094ce07168209c00cd5a7fb
mhanki <meike.hankewicz@protonmail.com>
2021-06-04 12:03:47 +0200

    cleanup

--- __tests__/utils/checkFiles.test.js
+++ __tests__/utils/checkFiles.test.js
@@ -1,17 +1,8 @@
 import dir from '../../__mocks__/directory.js'
 import { checkFiles } from '../../src/utils/checkFiles.js'
-import * as checks from '../../src/utils/searchDirectory.js'
-import * as prompts from '../../src/utils/userPrompts.js'
-import * as rewriteFiles from '../../src/utils/rewriteFiles.js'
 
 jest.mock('fs')
 jest.mock('fs/promises')
-
-const unmodifiedDir = {
-  "index.html": "content",
-  "readme.md": "content",
-  "app.js": "content"
-}
 
 const modifiedDir = {
   "index.html": "modified content",
@@ -23,15 +14,6 @@ const files = {
   "readme.md": { oldContent: "content", newContent: "new content" },
   "app.js": { oldContent: "content", newContent: "new content" }
 }
-
-const getModifiedSpy = jest.spyOn(checks, 'findModifiedFiles')
-const permissionSpy = jest.spyOn(prompts, 'getPermission')
-const rewriteAllSpy = jest.spyOn(rewriteFiles, 'rewriteAll')
-
-afterEach(() => { 
-  dir.reset()
-  jest.clearAllMocks()
-})
 
 describe("checkFiles", () => {
   dir.set(modifiedDir)
@@ -52,40 +34,3 @@ describe("checkFiles", () => {
     expect(unmodified).toEqual({"readme.md": { oldContent: "content", newContent: "new content" }})
   })
 })
-
-/* it("rewrites all files immediately if persmission is given", async () => {
-  dir.set(modifiedDir)
-  permissionSpy.mockReturnValueOnce(true)
-
-  await checkFiles(files)
-
-  expect(rewriteAllSpy).toHaveBeenCalledTimes(2)
-})
-
-it("prompts user for each file if permission is denied", async () => {
-  dir.set(modifiedDir)
-  permissionSpy
-    .mockReturnValueOnce(false)
-    .mockReturnValue(true)
-
-  await checkFiles(files)
-
-  expect(rewriteAllSpy).toHaveBeenCalledTimes(1)
-})
-
-
-it.only("rewrites file if permission is given", async () => {
-  dir.set(modifiedDir)
-  permissionSpy
-    .mockReturnValueOnce(false)
-    .mockReturnValueOnce(false)
-    .mockReturnValueOnce(true)
-
-  let res = await checkFiles(files)
-  console.log(res)
-  //expect(dir.get()).toEqual({})
-})
-
-it("does not rewrite file if persmission is denied", async () => {
-  
-}) */
