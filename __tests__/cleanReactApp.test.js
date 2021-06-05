d0edad228d8c1e82d7c4a9d330b38fb56cc26a65
mhanki <meike.hankewicz@protonmail.com>
2021-06-05 13:33:08 +0200

    add main process

---
+++ __tests__/cleanReactApp.test.js
@@ -0,0 +1,47 @@
+import dir from '../__mocks__/directory.js'
+import cleanReactApp from '../src/cleanReactApp.js'
+import * as checkDirectory from '../src/utils/checkDirectory.js'
+import * as cleanup from '../src/cleanup.js'
+
+jest.mock('fs')
+jest.mock('fs/promises')
+
+const checkDirectorySpy = jest.spyOn(checkDirectory, 'default')
+const cleanupSpy = jest.spyOn(cleanup, 'cleanup')
+const mockExit = jest.spyOn(process, 'exit')
+
+cleanupSpy.mockImplementation(() => new Promise((resolve, reject) => setTimeout(() => resolve(), 300)))
+mockExit.mockImplementation(() => {code: 1})
+
+afterEach(() => {
+  dir.reset()
+  jest.clearAllMocks()
+})
+
+it("exits if directory check fails", async () => {
+  checkDirectorySpy.mockImplementationOnce(() => false)
+
+  await cleanReactApp()
+
+  expect(cleanupSpy).toHaveBeenCalledTimes(0)
+  expect(mockExit).toHaveBeenCalledTimes(1)
+})
+
+it("starts cleanup if directory check succeeds", async () => {
+  checkDirectorySpy.mockImplementationOnce(() => true)
+
+  await cleanReactApp()
+
+  expect(checkDirectorySpy).toHaveBeenCalledTimes(1)
+  expect(cleanupSpy).toHaveBeenCalledTimes(1)
+  expect(mockExit).toHaveBeenCalledTimes(1)
+})
+
+it("prints start and exit messages", async () => {
+  const consoleSpy = jest.spyOn(console, 'log')
+  checkDirectorySpy.mockImplementationOnce(() => true)
+
+  await cleanReactApp()
+
+  expect(consoleSpy).toHaveBeenCalledTimes(3)
+})
--- src/cleanReactApp.js
+++ src/cleanReactApp.js
@@ -1,5 +1,19 @@
-const cleanReactApp = () => {
-  console.log('hola')
+import { printMessage } from './utils/userPrompts.js'
+import checkDirectory from './utils/checkDirectory.js'
+import { filesToRemove, filesToRewrite } from './files.js'
+import { cleanup } from './cleanup.js'
+
+const cleanReactApp = async () => {
+  printMessage(["Cleaning...  🧹💨"])
+
+  const start = await checkDirectory()
+
+  if(start){
+    await cleanup(filesToRemove, filesToRewrite)
+    printMessage(["✨ All done! ✨", " Happy coding!"])
+  }
+  
+  process.exit()
 }
 
 export default cleanReactApp
