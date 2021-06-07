507ba7b5d4998d27e5c4ccff12089e52fd4c7697
mhanki <meike.hankewicz@protonmail.com>
2021-06-07 15:50:21 +0200

    refactor rewriteFiles tests

--- __tests__/utils/rewriteFiles.test.js
+++ __tests__/utils/rewriteFiles.test.js
@@ -6,20 +6,39 @@ import { rewriteOne, rewriteAll, rewriteWithPermission } from '../../src/utils/r
 jest.mock('fs')
 jest.mock('fs/promises')
 
-afterEach(() => dir.reset())
+const getPermissionSpy = jest.spyOn(prompts, 'getPermission')
+
+const files = {
+  "index.html": { newContent: "new content"},
+  "index.css": { newContent: "new content"},
+  "app.js": { newContent: "new content"}
+}
+const directory = { "app.js": "content" }
+const updatedDirectory = {
+  "index.html": "new content",
+  "index.css": "new content",
+  "app.js": "new content"
+}
+
+beforeEach(() => { dir.set(directory)})
+
+afterEach(() => {
+  dir.reset()
+  jest.clearAllMocks()
+})
 
 describe("rewriteOne", () => {
   it("creates a new file if file doesn't exist", async () => {
-    expect(fs.existsSync("app.js")).toBe(false)
+    const newFile = {path: "index.js", data: "content"}
 
-    await rewriteOne("app.js", "content")
+    expect(fs.existsSync(newFile.path)).toBe(false)
 
-    expect(dir.get()).toEqual({ "app.js": "content" })
+    await rewriteOne(newFile.path, newFile.data)
+
+    expect(dir.get()).toEqual({...directory, [newFile.path]: newFile.data })
   })
   
   it("rewrites content of existing file", async () => {
-    dir.set({ "app.js": "content"})
-
     expect(fs.existsSync("app.js")).toBe(true)
 
     await rewriteOne("app.js", "updated content")
@@ -30,68 +49,29 @@ describe("rewriteOne", () => {
 
 describe("rewriteAll", () => {
   it("succesfully rewrites all files", async () => {
-    const files = {
-      "index.html": { newContent: "content"},
-      "index.css": { newContent: "more content"},
-      "app.js": { newContent: "updated content"}
-    }
-    dir.set({ "app.js": "content"})
-
-    expect(dir.get()).toEqual({ "app.js": "content"})
-
     let promiseArray = rewriteAll(files)
     await Promise.all(promiseArray)
 
-    expect(dir.get()).toEqual({
-      "index.html": "content",
-      "index.css": "more content",
-      "app.js": "updated content"
-    })
+    expect(dir.get()).toEqual(updatedDirectory)
   })
 })
 
 describe("rewriteWithPermission", () => {
   it("rewrites all files if permission is given", async () => {
-    const getPermissionSpy = jest.spyOn(prompts, 'getPermission')
     getPermissionSpy.mockReturnValue(true)
-
-    const files = {
-      "index.html": { newContent: "content"},
-      "index.css": { newContent: "more content"},
-      "app.js": { newContent: "updated content"}
-    }
-
-    dir.set({ "app.js": "content"})
-  
-    expect(dir.get()).toEqual({ "app.js": "content"})
   
     let promiseArray = await rewriteWithPermission(files)
     await Promise.all(promiseArray)
   
-    expect(dir.get()).toEqual({
-      "index.html": "content",
-      "index.css": "more content",
-      "app.js": "updated content"
-    })
+    expect(dir.get()).toEqual(updatedDirectory)
   })
 
   it("rewrites no files if permission is denied", async () => {
-    const getPermissionSpy = jest.spyOn(prompts, 'getPermission')
     getPermissionSpy.mockReturnValue(false)
-
-    const files = {
-      "index.html": { newContent: "content"},
-      "index.css": { newContent: "more content"},
-      "app.js": { newContent: "updated content"}
-    }
-
-    dir.set({ "app.js": "content"})
-  
-    expect(dir.get()).toEqual({ "app.js": "content"})
   
     let promiseArray = await rewriteWithPermission(files)
     await Promise.all(promiseArray)
   
-    expect(dir.get()).toEqual({ "app.js": "content"})
+    expect(dir.get()).toEqual(directory)
   })
 })
