805cf296cd2ff1ec2eb5e7de207eb965d0c94b90
mhanki <meike.hankewicz@protonmail.com>
2023-01-13 09:51:47 +0100

    Add FileProcessor class

---
+++ src/FileProcessor.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { writeFile, unlink } from 'fs/promises'
+import { FileInfo } from './FileInfo';
+
+export class FileProcessor {
+
+  readFiles = (paths: string[]): FileInfo[] => {
+    const files = paths.map((path) => ({
+      "filename": path,
+      "content": fs.readFileSync(path).toString()
+    }));
+  
+    return files;
+  }
+
+  rewriteOne = (file: FileInfo): Promise<void> => writeFile(file.filename, file.content);
+
+  rewriteAll = (files: FileInfo[]): Promise<void>[]=> files.map(file => 
+    writeFile(file.filename, file.content)
+  );
+
+  removeOne = (path: string): Promise<void> => unlink(path);
+
+  removeAll = (paths: string[]): Promise<void>[] => paths.map(path => this.removeOne(path));
+}
