8dc933752f73dab83773ca9c888250bfdd3b9a26
mhanki <meike.hankewicz@protonmail.com>
2023-01-19 13:29:51 +0100

    Update fs mock

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -6,7 +6,17 @@ const existsSync = (path) => dir.getOne(path) !== undefined;
 
 const readFileSync = (path) => dir.getOne(path);
 
+const readdirSync = (path) => {
+  return Object.keys(dir.getOne(path))
+}; 
+
+const statSync = (path) => ({
+  isDirectory: () => typeof dir.getOne(path) !== 'string'
+})
+
 fs.existsSync = existsSync;
 fs.readFileSync = readFileSync;
+fs.statSync = statSync;
+fs.readdirSync = readdirSync;
 
 export default fs;
